77b3eb1a55d63ed5d79df6433e5624c7b9a352e7
Freddy <46581440+FreddyisFreddy@users.noreply.github.com>
2019-05-25 21:26:56 -0400

    Add files via upload

---
+++ Potentiometer/Potentiometer.ino
@@ -0,0 +1,37 @@
+int sensorPin = A0;
+int ledPin = 13;
+
+void setup()
+{
+
+
+  pinMode(ledPin, OUTPUT);
+
+
+}
+
+
+void loop()
+{
+
+
+  int pot;
+
+
+
+  pot = analogRead(A0);
+
+
+
+  digitalWrite(13, HIGH);
+
+  delay(500);
+
+  digitalWrite(13, LOW);
+
+  delay(500);
+
+
+}
+
+
